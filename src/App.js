dbab3868682c3c567a16e13dee9d526dbd85416d
Nahuzacarias <zacarias.nahu@gmail.com>
2023-03-22 18:39:04 -0300

    preeentreganahuelZ

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Barradetareas from "./Components/NavBar/NavBar"
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ItemListContainer from './Components/ItemListContainer/ItemListContainer';
 
 const App = () => {
   return (
     
       <div className="App">
         <Barradetareas />
+        <ItemListContainer greeting= {"Bienvenidos a mi Tienda"}/>
         
       </div>
 
